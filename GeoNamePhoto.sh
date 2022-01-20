b2144aa21215fbbd4fa8849044bd080593ac34ae
Todd Duffin <1476397+TippyTurtle@users.noreply.github.com>
2022-01-20 10:52:58 -0800

    Update GeoNamePhoto.sh
    Added dependencies in comments.

--- GeoNamePhoto.sh
+++ GeoNamePhoto.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# You need to have the following installed:
+# exiftool
+# coreutils (for tr)
+# curl
+# jq
 
 # OutDir="/nfs/Public/FinishedPictures/"
 OutDir=~/Pictures/
@@ -37,16 +42,6 @@ for i in $(find . -type f -iname "*.jpg") ; do
     else
       echo GOOD Lat/Lon
       MapLoc=$(curl -s --retry 10 "https://nominatim.openstreetmap.org/reverse?format=json&zoom=10&email=tduffin@gmail.com&accept-language=en-us&lat=$lat&lon=$lon&zoom=18&addressdetails=1")
-
-      #
-      # "address":{"state":"South Cyprus","country":"Cyprus","country_code":"cy"}
-      # "address":{"state_district":"Greater London","state":"England","country":"United Kingdom","country_code":"gb"}
-      # "address":{"city":"Bellevue","county":"King County","state":"Washington","country":"United States","country_code":"us"}
-      # "address":{"municipality":"Council of the City of Sydney","state":"New South Wales","country":"Australia","country_code":"au"}
-      # "address":{"municipality":"Waverley Council","state":"New South Wales","country":"Australia","country_code":"au"}
-      # "address":{"municipality":"Torcy","county":"Seine-et-Marne","state":"Ile-de-France","region":"Metropolitan France","country":"France","country_code":"fr"}
-      # "address":{"municipality":"Le Raincy","county":"Seine-Saint-Denis","state":"Ile-de-France","region":"Metropolitan France","country":"France","country_code":"fr"}
-      #
 
       city=$(echo $MapLoc | jq '.address.city' | tr -d '"')
       echo "city " . "$city"
