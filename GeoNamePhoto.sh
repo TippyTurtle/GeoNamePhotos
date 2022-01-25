6e47833616ebb62962fe428e141688fb1e99f22c
Todd Duffin <1476397+TippyTurtle@users.noreply.github.com>
2022-01-25 12:55:47 -0800

    Update GeoNamePhoto.sh
    Removed need for TR. Cleaned up some NULL checking. Added more ways to find the date created. Added more Landmark fields to check.

--- GeoNamePhoto.sh
+++ GeoNamePhoto.sh
@@ -10,7 +10,6 @@
 
 # You need to have the following installed:
 # exiftool
-# coreutils (for tr)
 # curl
 # jq
 
@@ -19,114 +18,110 @@ OutDir=~/Pictures/
 email=YourEmail@Domain.ReplaceMe
 
 IFS=$'\n'
-for i in $(find . -type f \( -iname "*.mpg" -o -iname "*.mpeg" -o -iname "*.mov" -o -iname "*.mp4" -o -iname "*.wmv" -o -iname "*.avi" -o -iname "*.3gp" \)) ; do
-# for i in $(find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.cr2" \)) ; do
-  echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"."$i"."XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  lat=$(exiftool -gpslatitude -n "$i" | tr -d 'GPS Latitude : ')
-  lon=$(exiftool -gpslongitude -n "$i" | tr -d 'GPS Longitude : ')
-  
-  exiftool -n -DateTimeOriginal "$i"
-  echo Location:"lat=$lat&lon=$lon"
+# for i in $(find . -type f \( -iname "*.mpg" -o -iname "*.mpeg" -o -iname "*.mov" -o -iname "*.mp4" -o -iname "*.wmv" -o -iname "*.avi" -o -iname "*.3gp" \)) ; do
+for i in $(find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.cr2" \)) ; do
 
-  if [ "$lat" = "0.00000" ] ; then
-    echo Zero Latitude
-    exiftool -fixBase -v0 -P '-FileName<'"$OutDir"'${FileModifyDate}(${Model;}).%le' -d '%Y-%m//%Y-%m-%d %Hh%Mm%Ss%%-c' '-FileName<'"$OutDir"'${CreateDate}(${Model;}).%le' '-FileName<'"$OutDir"'${DateTimeOriginal}(${Model;}).%le' "$i"
-    if [ -e "$i" ]  ; then
-      exiftool -fixBase -v0 -P '-FileName<#${FileModifyDate}.%le' -d '%Y-%m//%Y-%m-%d %Hh%Mm%Ss%%-c' '-FileName<'"$OutDir"'${CreateDate}.%le' '-FileName<'"$OutDir"'${DateTimeOriginal}.%le' "$i"
-      echo No Camera Model 
-    fi
-    echo "No Location 1"
+    echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"."$i"."XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    lat=$(exiftool -GPSLatitude -j -n "$i" |  jq -r '.[0].GPSLatitude')
+    lon=$(exiftool -GPSLongitude -j -n "$i" |  jq -r '.[0].GPSLongitude')
 
-  else
+    exiftool -n -DateTimeOriginal "$i"
+    echo "lat=$lat&lon=$lon"
 
-    if [ -z "$lat" -o -z "$lon" -o "$lat" = 0 ] ; then
+    if [ -z "$lat" -o "$lat" = 0 -o "$lat" = 'null' ] ; then
       echo NULL Lon or Lat
-      # 'DateTimeOriginal' not defined
-      # 'CreateDate' not defined
 
-      exiftool -fixBase -v0 -P '-FileName<'"$OutDir"'${FileModifyDate}(${Model;}).%le' -d '%Y-%m//%Y-%m-%d %Hh%Mm%Ss%%-c' '-FileName<'"$OutDir"'${CreateDate}(${Model;}).%le' '-FileName<'"$OutDir"'${DateTimeOriginal}(${Model;}).%le' "$i"
+      exiftool -fixBase -v0 -P '-FileName<'"$OutDir"'${FileModifyDate}(${Model;}).%le' -d '%Y-%m//%Y-%m-%d %Hh%Mm%Ss%%-c' '-FileName<'"$OutDir"'${ModifyDate}(${Model;}).%le' '-FileName<'"$OutDir"'${ProfileDateTime}(${Model;}).%le' '-FileName<'"$OutDir"'${CreateDate}(${Model;}).%le' '-FileName<'"$OutDir"'${DateTimeOriginal}(${Model;}).%le' "$i"
       if [ -e "$i" ]  ; then
-       exiftool -fixBase -v0 -P '-FileName<'"$OutDir"'${FileModifyDate}.%le' -d '%Y-%m//%Y-%m-%d %Hh%Mm%Ss%%-c' '-FileName<'"$OutDir"'${CreateDate}.%le' '-FileName<'"$OutDir"'${DateTimeOriginal}.%le' "$i"
+       exiftool -fixBase -v0 -P '-FileName<'"$OutDir"'${FileModifyDate}.%le' -d '%Y-%m//%Y-%m-%d %Hh%Mm%Ss%%-c' '-FileName<'"$OutDir"'${ModifyDate}.%le' '-FileName<'"$OutDir"'${ProfileDateTime}.%le' '-FileName<'"$OutDir"'${CreateDate}.%le' '-FileName<'"$OutDir"'${DateTimeOriginal}.%le' "$i"
        echo No Cam Model 3
       fi
       echo "No Location 2"
     else
       echo GOOD Lat/Lon
       MapLoc=$(curl -s --retry 10 "https://nominatim.openstreetmap.org/reverse?format=json&zoom=10&email=$email&accept-language=en-US,en;q=0.5&lat=$lat&lon=$lon&zoom=19&addressdetails=1")
-#      echo "$MapLoc"
+      echo "$MapLoc"
 
-      city=$(echo $MapLoc | jq '.address.city' | tr -d '"')
+      city=$(echo $MapLoc |  jq -r '.address.city')
       echo "city" . "$city"
 
       if [ "$city" = 'null' ]; then
-          city=$(echo $MapLoc | jq '.address.town' | tr -d '"')
+          city=$(echo $MapLoc |  jq -r '.address.town')
           echo "town" . "$city"
       fi
       if [ "$city" = 'null' ]; then
-          city=$(echo $MapLoc | jq '.address.village' | tr -d '"')
+          city=$(echo $MapLoc |  jq -r '.address.village')
           echo "village" . "$city"
       fi
       if [ "$city" = 'null' ]; then
-          city=$(echo $MapLoc | jq '.address.hamlet' | tr -d '"')
+          city=$(echo $MapLoc |  jq -r '.address.hamlet')
           echo "hamlet" . "$city"
       fi
       if [ "$city" = 'null' ]; then
-          city=$(echo $MapLoc | jq '.address.suburb' | tr -d '"')
+          city=$(echo $MapLoc |  jq -r '.address.suburb')
           echo "suburb" . "$city"
       fi
       if [ "$city" = 'null' ]; then
-          city=$(echo $MapLoc | jq '.address.municipality' | tr -d '"')
+          city=$(echo $MapLoc |  jq -r '.address.municipality')
           echo "municipality" . "$city"
       fi
       if [ "$city" = 'null' ]; then
-          city=$(echo $MapLoc | jq '.address.state_district' | tr -d '"')
+          city=$(echo $MapLoc |  jq -r '.address.state_district')
           echo "state_district" . "$city"
       fi
       if [ "$city" = 'null' ]; then
-          city=$(echo $MapLoc | jq '.address.county' | tr -d '"')
+          city=$(echo $MapLoc |  jq -r '.address.county')
           echo "county" . "$city"
       fi
       if [ "$city" = 'null' ]; then
-          city=$(echo $MapLoc | jq '.address.state' | tr -d '"')
+          city=$(echo $MapLoc |  jq -r '.address.state')
           echo "state" . "$city"
       fi
       Landmark='null'
 
       # Find nearest Landmark. It gets overwritten with more and more specific place names, if they exist.
-      if [ $(echo $MapLoc | jq '.address.road' | tr -d '\"') != 'null' ]; then
-          Landmark="$(echo $MapLoc | jq '.address.road' | tr -d '\"')"
+      if [ $(echo $MapLoc |  jq -r '.address.suburb') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.suburb')"
+          echo suburb . "$Landmark"
+      fi
+      if [ $(echo $MapLoc |  jq -r '.address.quarter') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.quarter')"
+          echo quarter . "$Landmark"
+      fi
+      if [ $(echo $MapLoc |  jq -r '.address.road') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.road')"
           echo road . "$Landmark"
       fi
-      if [ $(echo $MapLoc | jq '.address.neighbourhood' | tr -d '\"') != 'null' ]; then
-          Landmark="$(echo $MapLoc | jq '.address.neighbourhood' | tr -d '\"')"
+      if [ $(echo $MapLoc |  jq -r '.address.neighbourhood') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.neighbourhood')"
           echo neighbourhood . "$Landmark"
       fi
-      if [ $(echo $MapLoc | jq '.address.place' | tr -d '\"') != 'null' ]; then
-          Landmark="$(echo $MapLoc | jq '.address.place' | tr -d '\"')"
+      if [ $(echo $MapLoc |  jq -r '.address.place') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.place')"
           echo place . "$Landmark"
       fi
-      if [ $(echo $MapLoc | jq '.address.building' | tr -d '\"') != 'null' ]; then
-          Landmark="$(echo $MapLoc | jq '.address.building' | tr -d '\"')"
+      if [ $(echo $MapLoc |  jq -r '.address.building') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.building')"
           echo building . "$Landmark"
       fi
-      if [ $(echo $MapLoc | jq '.address.office' | tr -d '\"') != 'null' ]; then
-          Landmark="$(echo $MapLoc | jq '.address.office' | tr -d '\"')"
+      if [ $(echo $MapLoc |  jq -r '.address.office') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.office')"
           echo office . "$Landmark"
       fi
-      if [ $(echo $MapLoc | jq '.address.man_made' | tr -d '\"') != 'null' ]; then
-          Landmark="$(echo $MapLoc | jq '.address.man_made' | tr -d '\"')"
+      if [ $(echo $MapLoc |  jq -r '.address.man_made') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.man_made')"
           echo man_made . "$Landmark"
       fi
-      if [ $(echo $MapLoc | jq '.address.amenity' | tr -d '\"') != 'null' ]; then
-          Landmark="$(echo $MapLoc | jq '.address.amenity' | tr -d '\"')"
+      if [ $(echo $MapLoc |  jq -r '.address.amenity') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.amenity')"
           echo amenity . "$Landmark"
       fi
-      if [ $(echo $MapLoc | jq '.address.historic' | tr -d '\"') != 'null' ]; then
-          Landmark="$(echo $MapLoc | jq '.address.historic' | tr -d '\"')"
+      if [ $(echo $MapLoc |  jq -r '.address.historic') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.historic')"
           echo historic . "$Landmark"
       fi
-      if [ $(echo $MapLoc | jq '.address.tourism' | tr -d '\"') != 'null' ]; then
-          Landmark="$(echo $MapLoc | jq '.address.tourism' | tr -d '\"')"
+      if [ $(echo $MapLoc |  jq -r '.address.tourism') != 'null' ]; then
+          Landmark="$(echo $MapLoc |  jq -r '.address.tourism')"
           echo tourism . "$Landmark"
       fi
 
@@ -134,12 +129,16 @@ for i in $(find . -type f \( -iname "*.mpg" -o -iname "*.mpeg" -o -iname "*.mov"
       if [ "$city" = 'null' ]; then
           city="$Landmark"
       else
-          city="$Landmark-$city"
+          if [ "$Landmark" = 'null' ]; then
+              city="$city"
+          else
+              city="$Landmark-$city"
+          fi
       fi
 
       # Last ditch effort to have some type of location in the filename.
       if [ "$city" = 'null' ]; then
-          city=$(echo $MapLoc | jq '.address.country' | tr -d '"')
+          city=$(echo $MapLoc |  jq -r '.address.country')
           echo "country" . "$city"
       fi
 
@@ -151,21 +150,19 @@ for i in $(find . -type f \( -iname "*.mpg" -o -iname "*.mpeg" -o -iname "*.mov"
           echo $MapLoc >> GeoNameCityError.txt
       fi
 
-      country=$(echo $MapLoc | jq '.address.country' | tr -d '"')
+      country=$(echo $MapLoc |  jq -r '.address.country')
       echo 
       echo "City is $city in the country $country"
 
-      exiftool -fixBase -v0 -P '-FileName<'"$OutDir"'${FileModifyDate}('"$city"')(${Model;}).%le' '-FileName<'"$OutDir"'${CreateDate}('"$city"')(${Model;}).%le' '-FileName<'"$OutDir"'${DateTimeOriginal}('"$city"')(${Model;}).%le' -d '%Y-%m/'"$country"'/%Y-%m-%d %Hh%Mm%Ss%%-c' "$i"
+      exiftool -fixBase -v0 -P '-FileName<'"$OutDir"'${FileModifyDate}('"$city"')(${Model;}).%le' '-FileName<'"$OutDir"'${ModifyDate}('"$city"')(${Model;}).%le' '-FileName<'"$OutDir"'${ProfileDateTime}('"$city"')(${Model;}).%le' '-FileName<'"$OutDir"'${CreateDate}('"$city"')(${Model;}).%le' '-FileName<'"$OutDir"'${DateTimeOriginal}('"$city"')(${Model;}).%le' -d '%Y-%m/'"$country"'/%Y-%m-%d %Hh%Mm%Ss%%-c' "$i"
       if [ -e "$i" ] ; then
-        exiftool -fixBase -v0 -P '-FileName<'"$OutDir"'${FileModifyDate}('"$city"').%le' '-FileName<'"$OutDir"'${CreateDate}('"$city"').%le' '-FileName<'"$OutDir"'${DateTimeOriginal}('"$city"').%le' -d '%Y-%m/'"$country"'/%Y-%m-%d %Hh%Mm%Ss%%-c' "$i"
+        exiftool -fixBase -v0 -P '-FileName<'"$OutDir"'${FileModifyDate}('"$city"').%le' '-FileName<'"$OutDir"'${ModifyDate}('"$city"').%le' '-FileName<'"$OutDir"'${ProfileDateTime}('"$city"').%le' '-FileName<'"$OutDir"'${CreateDate}('"$city"').%le' '-FileName<'"$OutDir"'${DateTimeOriginal}('"$city"').%le' -d '%Y-%m/'"$country"'/%Y-%m-%d %Hh%Mm%Ss%%-c' "$i"
         echo No Camera Model 2
       fi
-
       # Do not remove this. It would be evil to OpenStreetMaps and against their terms of use.  Consider making it much longer and running overnight.
       echo Sleeping 2 seconds...
-      sleep 2
+      sleep 20
     fi
-  fi
 done
 
 find . -type d -empty -delete
